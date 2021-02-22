c5d787bb782f8a60b31ae7b2b353c72af5103528
mizugoro <mizugoro0121@outlook.jp>
2021-02-22 15:11:33 +0900

    first commit

---
+++ FirstDay/sum.c
@@ -0,0 +1,21 @@
+/*/OneYearProject/FirstDay/sum.c*/
+/*terminal上で入力した数を加算したものを出力するプログラム*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[])
+{
+    /*argcは入力数,argv[]は入力されたもの*/
+    int sum = 0;
+
+    for (int i = 1; i < argc; i++)
+    {
+        int value = 0;
+
+        value = atoi(argv[i]);
+        sum += value;
+    }
+    printf("出力結果は%dです\n", sum);
+    return 0;
+}
