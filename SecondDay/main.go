a550fb2e01178d02fe1d520e964a081000fb21a2
mizugoro <mizugoro0121@outlook.jp>
2021-03-05 16:53:01 +0900

    operate database file and create table

---
+++ SecondDay/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func execDB(db *sql.DB, q string) {
+	if _, err := db.Exec(q); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	q := `
+        CREATE TABLE memo (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          body VARCHAR(255) NOT NULL,
+          created_at TIMESTAMP DEFAULT (DATETIME('now','localtime'))
+        )
+    `
+	execDB(db, q)
+
+	q = `
+        INSERT INTO memo(body)
+        VALUES ('body1'), ('body2')
+    `
+	execDB(db, q)
+
+	db.Close()
+}
